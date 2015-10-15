5982daa10937a88147e45f38a39e34a6dca189c2
Andrew Osheroff <andrewosh@gmail.com>
2015-10-15 16:25:47 -0400

    Fixed up binder-yml handling

--- dependencies/binder-yml/handler.js
+++ dependencies/binder-yml/handler.js
@@ -15,34 +15,43 @@ BinderYml.prototype._generateString = function (name, contents) {
   // the current handling does not take the front-end OR language
   // into account
 
+  //console.log("binder.yml: {0}".format(JSON.stringify(contents)))
   var handleEnv = function () {
     var strings = _.map(contents.env, function (env) {
       return _.keys(env).map(function (key) {
         return 'ENV {0} {1}\n'.format(key, env[key])
       })
     })
-    return _.flatten(strings)
+    return _.flatten(strings).join('')
   }
 
   var handleApt = function () {
-    var strings = _.map(contents.apt, function (apt) {
-      return 'RUN apt-get install {0}\n'.format(apt)
-    })
-    return strings
+    if (contents.apt) {
+      var packages = contents.apt.join(' ')
+      var installString = '' +
+        'RUN apt-get update -y && ' +
+        'apt-get install -y {0} && ' +
+        'apt-get clean && ' +
+        'rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*tmp\n'
+      return installString.format(packages)
+    }
+    return ''
   }
 
   var handleInstall = function () {
     var strings = _.map(contents.install, function (install) {
       return 'RUN {0}\n'.format(install)
     })
-    return strings
+    return strings.join('')
   }
 
   return [
+    'ADD * $HOME/notebooks/\n',
+    'USER root\n',
     handleEnv(),
     handleApt(),
-    handleInstall(),
-    ["USER main\n"]
+    'USER main\n',
+    handleInstall()
   ].join('\n')
 }
 
