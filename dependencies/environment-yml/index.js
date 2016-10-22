6dd8ca8403ed3113fa71c39318daa342615a3e0a
Björn Dahlgren <bjodah@gmail.com>
2016-10-22 10:38:03 +0200

    Export {LIBRARY,C_INCLUDE,CPLUS_INCLUDE}_PATH

--- dependencies/environment-yml/index.js
+++ dependencies/environment-yml/index.js
@@ -15,6 +15,9 @@ EnvironmentYml.prototype._generateString = function (name, contents) {
   'ADD {0} environment.yml\n'.format(name) +
   'RUN conda env create -n binder\n' +
   'RUN echo \"export PATH=/home/main/anaconda2/envs/binder/bin/:/home/main/anaconda3/envs/binder/bin/:$PATH\" >> ~/.binder_start\n' +
+  'RUN echo \"export LIBRARY_PATH=/home/main/anaconda2/envs/binder/lib:/home/main/anaconda3/envs/binder/lib:$LIBRARY_PATH\" >> ~/.binder_start\n' +
+  'RUN echo \"export C_INCLUDE_PATH=/home/main/anaconda2/envs/binder/include:/home/main/anaconda3/envs/binder/include:$C_INCLUDE_PATH\" >> ~/.binder_start\n' +
+  'RUN echo \"export CPLUS_INCLUDE_PATH=/home/main/anaconda2/envs/binder/include:/home/main/anaconda3/envs/binder/include:$CPLUS_INCLUDE_PATH\" >> ~/.binder_start\n' +
   'RUN conda install -n binder jupyter\n' +
   'RUN /bin/bash -c \"source activate binder && jupyter kernelspec install-self --user\"\n'
   return str
