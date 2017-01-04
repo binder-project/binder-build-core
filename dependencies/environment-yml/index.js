33940c77ccd1ee35cfe9f9c52d96725bf0934e37
Stephen W. Nuchia <snuchia@gmail.com>
2017-01-04 15:43:00 -0800

    oops

--- dependencies/environment-yml/index.js
+++ dependencies/environment-yml/index.js
@@ -17,7 +17,7 @@ EnvironmentYml.prototype._generateString = function (name, contents) {
   'RUN echo \"export PATH=/home/main/anaconda2/envs/binder/bin/:/home/main/anaconda3/envs/binder/bin/:$PATH\" >> ~/.binder_start\n' +
   'RUN conda install -n binder jupyter\n' +
   'RUN /bin/bash -c \"source activate binder && python -m ipykernel install --user\"\n'
-  'RUN echo \"c.NotebookApp.token = \'\'\" >> /home/main/.jupyter/jupyter_notebook_config.py
+  'RUN echo \"c.NotebookApp.token = \'\'\" >> /home/main/.jupyter/jupyter_notebook_config.py'
   return str
 }
 
